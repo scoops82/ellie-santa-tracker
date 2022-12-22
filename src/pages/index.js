3980564799b5aadf3cf04dd6c813e4e016d7dc23
scoops82 <rupert.taylor1982@gmail.com>
2022-12-22 18:41:25 +0000

    end of tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,152 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '@components/Layout';
-import Section from '@components/Section';
-import Container from '@components/Container';
-import Map from '@components/Map';
-import Button from '@components/Button';
+import Layout from "@components/Layout";
+import Section from "@components/Section";
+import Container from "@components/Container";
+import Map from "@components/Map";
+import Button from "@components/Button";
 
-import styles from '@styles/Home.module.scss';
+import styles from "@styles/Home.module.scss";
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+import useSWR from "swr";
+
+const DEFAULT_CENTER = [38.907132, -77.036546];
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Home() {
+  const { data, error, isLoading } = useSWR(
+    "https://firebasestorage.googleapis.com/v0/b/santa-tracker-firebase.appspot.com/o/route%2Fsanta_en.json?alt=media&2018b",
+    fetcher
+  );
+
+  const currentDate = new Date(Date.now());
+  // const currentDate = new Date("2022-12-25T00:15:30.115Z");
+  const currentYear = currentDate.getFullYear();
+
+  const destinations = data?.destinations.map((destination) => {
+    const { arrival, departure } = destination;
+
+    const arrivalDate = new Date(arrival);
+    const departureDate = new Date(departure);
+
+    arrivalDate.setFullYear(currentYear);
+    departureDate.setFullYear(currentYear);
+
+    return {
+      ...destination,
+      arrival: arrivalDate.getTime(),
+      departure: departureDate.getTime(),
+    };
+  });
+
   return (
     <Layout>
       <Head>
         <title>Next.js Leaflet Starter</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
+        <meta
+          name="description"
+          content="Create mapping apps with Next.js Leaflet Starter"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Section>
         <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter
-          </h1>
+          <h1 className={styles.title}>Next.js Leaflet Starter</h1>
 
-          <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
+          <Map
+            className={styles.homeMap}
+            width="800"
+            height="400"
+            center={[0, 0]}
+            zoom={2}
+          >
+            {({ TileLayer, Marker, Popup }, Leaflet) => (
               <>
                 <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
+
+                {destinations?.map(
+                  ({ id, arrival, departure, location, city, region }) => {
+                    const arrivalDate = new Date(arrival);
+                    const arrivalHours = arrivalDate.getHours();
+                    const arrivalMinutes = arrivalDate.getMinutes();
+                    const arrivalTime = `${arrivalHours}:${arrivalMinutes}`;
+
+                    const departureDate = new Date(departure);
+                    const departureHours = departureDate.getHours();
+                    const departureMinutes = departureDate.getMinutes();
+                    const departureTime = `${departureHours}:${departureMinutes}`;
+
+                    const santaWasHere =
+                      currentDate.getTime() - departureDate.getTime() > 0;
+                    const santaIsHere =
+                      currentDate.getTime() - arrivalDate.getTime() > 0 &&
+                      !santaWasHere;
+
+                    let iconUrl = "/images/tree-marker-icon.png";
+                    let iconRetinaUrl = "/images/tree-marker-icon-2x.png";
+
+                    if (santaWasHere) {
+                      iconUrl = "/images/gift-marker-icon.png";
+                      iconRetinaUrl = "/images/gift-marker-icon-2x.png";
+                    }
+
+                    if (santaIsHere) {
+                      iconUrl = "/images/santa-marker-icon.png";
+                      iconRetinaUrl = "/images/santa-marker-icon-2x.png";
+                    }
+
+                    let className = "";
+
+                    if (santaIsHere) {
+                      className = `${className} ${styles.iconSantaIsHere}`;
+                    }
+
+                    return (
+                      <Marker
+                        key={id}
+                        position={[location.lat, location.lng]}
+                        icon={Leaflet.icon({
+                          iconUrl,
+                          iconRetinaUrl,
+                          iconSize: [41, 41],
+                          className,
+                        })}
+                      >
+                        <Popup>
+                          {city}, {region}
+                          <br />
+                          Arrival: {arrivalDate.toDateString()} @ {arrivalTime}
+                          <br />
+                          Departure: {departureDate.toDateString()} @{" "}
+                          {departureTime}
+                        </Popup>
+                      </Marker>
+                    );
+                  }
+                )}
               </>
             )}
           </Map>
 
           <p className={styles.description}>
-            <code className={styles.code}>yarn create next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
+            <code className={styles.code}>
+              yarn create next-app -e
+              https://github.com/colbyfayock/next-leaflet-starter
+            </code>
           </p>
 
           <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
+            <Button href="https://github.com/colbyfayock/next-leaflet-starter">
+              Vew on GitHub
+            </Button>
           </p>
         </Container>
       </Section>
     </Layout>
-  )
+  );
 }
